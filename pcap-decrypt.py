c53d110bf3a25c2638c274df5f4578c425979aa7
Wolfgang Frisch <wfr+dev@roembden.net>
2017-02-14 07:51:56 +0100

    Add list of Xiaomi MAC addresses
    + improve output readability

--- pcap-decrypt.py
+++ pcap-decrypt.py
@@ -31,6 +31,18 @@ except ImportError:
 import miio
 
 
+# https://code.wireshark.org/review/gitweb?p=wireshark.git;a=blob_plain;f=manuf
+# |grep -i xiaomi |sort |awk '{ print "\""$1"\","  }'
+MAC_PREFIXES_XIAOMI = frozenset([
+    "00:9E:C8", "0C:1D:AF", "10:2A:B3", "14:F6:5A", "18:59:36", "20:82:C0",
+    "28:6C:07", "28:E3:1F", "34:80:B3", "34:CE:00", "38:A4:ED", "3C:BD:3E",
+    "58:44:98", "64:09:80", "64:B4:73", "64:CC:2E", "68:DF:DD", "74:23:44",
+    "74:51:BA", "78:02:F8", "7C:1D:D9", "8C:BE:BE", "98:FA:E3", "9C:99:A0",
+    "A0:86:C6", "AC:C1:EE", "AC:F7:F3", "B0:E2:35", "C4:0B:CB", "C4:6A:B7",
+    "D4:97:0B", "F0:B4:29", "F4:8B:32", "F8:A4:5F", "FC:64:BA"
+])
+
+
 def get_macs(packet):
     """Get the MAC addresses from a PyShark packet.
     Returns (source MAC, destination MAC)
@@ -51,46 +63,42 @@ parser.add_argument("--print-headers", action='store_true')
 parser.add_argument("--print-raw", action='store_true')
 args = parser.parse_args()
 
-
-XIAOMI_MAC = "f0:b4:29"
 cap = pyshark.FileCapture(
-    args.pcapfile,
-    display_filter=(
-        "(wlan.addr[0:3]=={0} || eth.addr[0:3]=={0}) "
-        "&& udp.port == 54321".format(XIAOMI_MAC)))
+    args.pcapfile, display_filter=("udp.port == 54321"))
 
 device_token = {}  # type: Dict[str, bytes]
 
 for packet in cap:
-    mac_src, mac_dst = get_macs(packet)
-    incoming = mac_src.startswith(XIAOMI_MAC)
-    outgoing = not incoming
-    if not "data" in packet:
+    if "data" not in packet:
         continue
+    if (not ipaddress.ip_address(packet.ip.src).is_private
+            or not ipaddress.ip_address(packet.ip.dst).is_private):
+        print("NOT IMPLEMENTED: packet to/from Xiaomi Cloud")
+        continue
+
+    mac_src, mac_dst = get_macs(packet)
+    incoming = mac_src.upper()[0:8] in MAC_PREFIXES_XIAOMI
+    outgoing = mac_dst.upper()[0:8] in MAC_PREFIXES_XIAOMI
     packet.data.raw_mode = True
     data = bytearray.fromhex(packet.data.data)
     mp = miio.MiioPacket()
     mp.read(data)
 
-    print("\n### {0} ({1}) => {2} ({3})".format(
-        packet.ip.src, mac_src, packet.ip.dst, mac_dst))
+    print("\n### {0} => {1} ({2} => {3})".format(
+        packet.ip.src, packet.ip.dst, mac_src, mac_dst))
     if args.print_raw:
         print("RAW: %s" % packet.data.data)
     if args.print_headers:
         print("HEADER:")
         miio.print_head(data)
 
-    if not ipaddress.ip_address(packet.ip.src).is_private \
-        or not ipaddress.ip_address(packet.ip.dst).is_private:
-        print("NOT IMPLEMENTED: packet to/from Xiaomi Cloud")
-        continue
-
-    decrypted = None       
+    decrypted = None
     if incoming:
         if len(mp.data) == 0:
             token = mp.md5
             device_token[mac_src] = token
-            print("META: device {0} has token: {1}".format(mac_src, token.hex()))
+            print("META: device {0} has token: {1}".format(
+                mac_src, token.hex()))
         elif mac_src in device_token:
             decrypted = miio.decrypt(device_token[mac_src], data)
     elif outgoing:
@@ -99,6 +107,6 @@ for packet in cap:
         elif mac_dst in device_token:
             decrypted = miio.decrypt(device_token[mac_dst], data)
     if decrypted:
-        print("DECRYPTED: %s" % decrypted.decode('UTF-8'))
+        print(decrypted.decode('UTF-8'))
 
 # vim:set expandtab tabstop=4 shiftwidth=4 softtabstop=4 nowrap:
